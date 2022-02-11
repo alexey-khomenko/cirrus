07f4f4d4e57f34afd46fb10e398ae9d7bc4c2470
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2022-02-11 17:53:24 +0200

    up

--- docs/js/page/index/slider.js
+++ docs/js/page/index/slider.js
@@ -3,22 +3,13 @@ document.addEventListener('DOMContentLoaded', function () {
   const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
-    autoplay: {
-      delay: 5000,
-    },
+    // autoplay: {
+    //   delay: 5000,
+    // },
     on: {
       activeIndexChange: function () {
         setTimeout(() => {
           const slide = document.querySelector('.slider .swiper-slide-active');
-
-          // const titleObj = document.querySelector('.slider .title');
-          // const subTitleObj = document.querySelector('.slider .sub-title');
-          // const linkObj = document.querySelector('.slider .link');
-          //
-          // titleObj.textContent = slide.dataset.title;
-          // subTitleObj.textContent = slide.dataset.subTitle;
-          // linkObj.href = slide.dataset.link;
-
           const bullets = document.querySelectorAll('.slider .bullet');
           const index = +slide.dataset.swiperSlideIndex;
 
@@ -42,6 +33,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
     swiper.slideToLoop(+bullet.dataset.index);
     swiper.autoplay.start();
+  });
+
+  document.addEventListener('click', function (e) {
+    const arrow = e.target.closest('.slider .arrow-left');
+
+    if (!arrow) return true;
+
+    console.log('todo left');
+  });
+
+  document.addEventListener('click', function (e) {
+    const arrow = e.target.closest('.slider .arrow-right');
+
+    if (!arrow) return true;
+
+    console.log('todo right');
   });
 
 });
